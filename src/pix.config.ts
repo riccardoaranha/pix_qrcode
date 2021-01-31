56d42978cdb93edb5b43ccc2ad224d733511739c
riccardoaranha <riccardoaranha@gmail.com>
2021-01-31 03:07:47 +0100

    Deletion of files and files that chenged folder location for better organization.  Changes to be committed: 	deleted:    assets/logo.png 	deleted:    src/pix.config.ts 	deleted:    src/pix.fields.ts 	deleted:    src/pix.groups.ts 	deleted:    src/pix.messages.ts 	deleted:    src/pix.qrcode.ts 	deleted:    src/pix.ts 	deleted:    src/pix.utils.ts 	deleted:    testpage.html

--- src/pix.config.ts
+++
@@ -1,9 +0,0 @@
-enum ValidationTypes {
-		None = 0,
-		Full = 1
-	}
-
-var ID_LENGHT : number = 2;
-var ValidationType:ValidationTypes = ValidationTypes.Full;
-
-export { ValidationTypes, ID_LENGHT, ValidationType }
