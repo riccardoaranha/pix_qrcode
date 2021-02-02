106068c4e9a82b416e0d5eace4880a38bae0d614
Riccardo ARANHA <riccardo.aranha@valeo.com>
2021-02-02 16:18:18 +0100

    Change of endpoint for production  Changes to be committed: 	modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
-      API_URL: 'http://localhost:3000/api',
-      //API_URL: 'https://pix-qrcode.vercel.app/api',
+      //API_URL: 'http://localhost:3000/api',
+      API_URL: 'https://pix-qrcode.vercel.app/api',
     },
     async headers() {
     return [
@@ -11,8 +11,7 @@ module.exports = {
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
             { key: "Access-Control-Allow-Origin", value: "*" },
-            //{ key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-            { key: "Access-Control-Allow-Methods", value: "GET, POST" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
             { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
           ]
         }
